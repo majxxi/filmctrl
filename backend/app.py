441a7ef77d1111b29a0f0652610d781462b08f15
majick <majicktadepa@gmail.com>
2020-12-04 12:45:25 -0800

    hide api key

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,7 @@ connect_db(app)
 
 API_URL = "https://movie-database-imdb-alternative.p.rapidapi.com"
 API_HEADERS = {
-  'x-rapidapi-key': 'ffd59e5628msh60a12b2fa4ad1d0p17ffaajsn3c45cec465b1',
+  'x-rapidapi-key': os.getenv("API_KEY"),
   'x-rapidapi-host': 'movie-database-imdb-alternative.p.rapidapi.com'
 }
 
--- backend/secrets.py
+++
@@ -1,6 +0,0 @@
-API_URL = "https://movie-database-imdb-alternative.p.rapidapi.com"
-
-API_HEADERS = {
-  'x-rapidapi-key': 'ffd59e5628msh60a12b2fa4ad1d0p17ffaajsn3c45cec465b1',
-  'x-rapidapi-host': 'movie-database-imdb-alternative.p.rapidapi.com'
-}
